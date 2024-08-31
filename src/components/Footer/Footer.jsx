4826b607f73f3c5bc4ee65f88f379aa0638bd019
tamer <ahmedssworkgit@gmail.com>
2024-08-31 16:18:15 +0300

    the new edit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,7 @@ import style from "./Footer.module.css"
 
 export default function Footer() {
   return <>
-<div className='fixed bottom-0 left-0 right-0 bg-emerald-200 p-4'>
-  <h1 className='text-slate-100 font-bold text-lg'>Footer</h1>
-</div>
+
   </>
     
   
